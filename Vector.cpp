ac4301f1279951c2b83c8b510eccdf3d1fb4815a
Danil Domnich <domni4.d2@yandex.ru>
2025-02-17 16:59:28 +0300

    Добавить класс и необходимые конструкторы, изменить кодировку

---
+++ Vector.cpp
@@ -0,0 +1,30 @@
+﻿#include "Vector.hpp"
+
+Vector::Vector()
+{
+	n = 0;
+	array = nullptr;
+}
+
+Vector::Vector(const Vector& other)
+{
+	n = other.n;
+	array = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		array[i] = other.array[i];
+	}
+}
+
+Vector::Vector(Vector&& other)
+{
+	n = other.n;
+	array = other.array;
+	other.array = nullptr;
+	other.n = 0;
+}
+
+Vector::~Vector()
+{
+	delete[] array;
+}
---
+++ Vector.hpp
@@ -0,0 +1,14 @@
+﻿#ifndef VECTOR_HPP
+#define VECTOR_HPP
+class Vector
+{
+private:
+	int* array;
+	unsigned int n;
+public:
+	Vector();
+	Vector(const Vector&);
+	Vector(Vector&&);
+	~Vector();
+};
+#endif
